8a8175f087b002b7c0ece0a9a5534dbbdd1a7169
Juan Erazo <fersot100@yahoo.com>
2017-11-30 22:13:06 -0800

    fixed mobile stuff

--- topic-switcher.js
+++ topic-switcher.js
@@ -1,27 +1,27 @@
-const projects = document.querySelector('#projects');
-const skills = document.querySelector('#skills');
-const experience = document.querySelector('#experience');
-const about = document.querySelector('#about');
+// const projects = document.querySelector('#projects');
+// const skills = document.querySelector('#skills');
+// const experience = document.querySelector('#experience');
+// const about = document.querySelector('#about');
 
-//Put all views into a list
-const nodeList = [projects, skills, experience, about];
-nodeList.forEach(node => {node.style.display = 'none'})
-projects.style.display = 'block';
+// //Put all views into a list
+// const nodeList = [projects, skills, experience, about];
+// nodeList.forEach(node => {node.style.display = 'none'})
+// projects.style.display = 'block';
 
-const switchView = event => {
-    const newView = event.target.dataset.value;
-    console.log(newView);
-    nodeList.forEach(node => {
-        if(node.id === newView){
-            node.style.display = 'block';
-        }else{ 
-            node.style.display = 'none';
-        }
-    });
-}
+// const switchView = event => {
+//     const newView = event.target.dataset.value;
+//     console.log(newView);
+//     nodeList.forEach(node => {
+//         if(node.id === newView){
+//             node.style.display = 'block';
+//         }else{ 
+//             node.style.display = 'none';
+//         }
+//     });
+// }
 
-//Get navs
-const navs = document.getElementById('navs');
-navs.addEventListener('click', switchView);
+// //Get navs
+// const navs = document.getElementById('navs');
+// navs.addEventListener('click', switchView);
 
-window.addEventListener('resize')
+// window.addEventListener('resize')
